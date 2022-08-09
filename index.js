79cdc127dc45ebbe2312574c8dd1130857e7ed01
FernandoTol <toledo.lufe@gmail.com>
2022-08-08 20:37:51 -0500

    add calculadora

---
+++ index.js
@@ -0,0 +1,8 @@
+const operacion = () => {
+    const primerValor = document.getElementById( 'Pvalor' ).value
+    const segundoValor = document.getElementById( 'Svalor' ).value
+    const calculadora = document.getElementById( 'calculadora' ).value
+
+    
+
+}
